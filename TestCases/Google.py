7d7704e4494692530d8c311163155534e5f38b5b
Dhiraj <Dhirajquality@gmail.com>
2022-04-26 23:09:02 +0530

    Google Page Created

---
+++ TestCases/Google.py
@@ -0,0 +1,9 @@
+import pytest
+from selenium import webdriver
+
+
+def test_one():
+    driver = webdriver.Firefox(executable_path='E:\\GitPractice\\Drivers\\geckodriver.exe')
+    driver.get('https://www.google.co.in')
+    print("Page Title is : %s" % driver.title)
+    # driver.quit()
