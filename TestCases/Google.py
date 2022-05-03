be9d452f570c62031826be0b96c888164146f2b4
Dhiraj <Dhirajquality@gmail.com>
2022-05-03 14:26:52 +0530

    3rd may commit

--- TestCases/Google.py
+++ TestCases/Google.py
@@ -1,7 +1,7 @@
 import pytest
 from selenium import webdriver
 
-
+@pytest.mark.parametrize()
 def test_one():
     driver = webdriver.Firefox(executable_path='E:\\GitPractice\\Drivers\\geckodriver.exe')
     driver.get('https://www.google.co.in')
